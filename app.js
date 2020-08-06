1159a80b48ab87035ae2153d799425ecd0be37fb
Haiyang Hong <hong961127@gmail.com>
2020-08-06 09:29:23 -0700

    Setup apollo server express

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: "./config/config.env" });
+require("./config/db")();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
